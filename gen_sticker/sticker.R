f75b2f8c670337b4e6e3ebab58404d47e81104a5
David Schweizer <schweizer.david@gmx.de>
2023-09-06 14:40:56 +0200

    rough version

--- gen_sticker/sticker.R
+++ gen_sticker/sticker.R
@@ -6,67 +6,47 @@ library(sysfonts)
 library(gapminder)
 library(tidyverse)
 
-gapminder_cleaned <- gapminder %>% 
-  filter(year == "2007") %>% 
-  mutate(
-    pop2 = pop + 1,
-    continent = case_when(
-      continent == "Oceania" ~ "Asia",
-      TRUE ~ as.character(continent)
-    ) %>% as.factor %>% 
-      fct_relevel("Asia", "Americas", "Europe", "Africa")
-  ) 
+gapminder <- gapminder
 
-plot <- ggplot(data = gapminder_cleaned, aes(x = gdpPercap, y = lifeExp)) +
-  geom_point(aes(size = pop, color = continent)) +
-  geom_point(aes(size = pop2), color = "black", shape = 21) +
-  scale_x_log10(breaks = c(500, 1000, 2000, 4000,
-                           8000, 16000, 32000, 64000)) +
-  scale_y_continuous(breaks = seq(0, 90, by = 10)) +
-  scale_color_manual(values = c("#F15772", "#7EEB03",
-                                "#FBE700", "#54D5E9"))  +
-  #scale_size_continuous(range = c(1, 30)) +
+gapminder_cleaned <- gapminder %>% 
+  #filter(year == "2007") %>% 
+  filter(continent == "Africa" | continent == "Europe")
+
+plot <- ggplot(data = gapminder_cleaned, aes(x = year, y = lifeExp)) +
+  geom_jitter(alpha = 0.1, aes(color = continent)) +
+  geom_smooth(aes(color = continent), alpha = 0.7) +
   guides(size = FALSE, color = FALSE) +
    labs(
      x = "",
      y = ""
    ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(panel.grid.major=element_line(colour="black"),
+        panel.grid.minor=element_blank())
 
 #stripes <- image_read("images/climate_stripes.png")
-
-p <- ggplot(aes(x = mpg, y = wt), data = mtcars) + geom_point()
-p <- p + theme_void() + theme_transparent()
-
-sticker(p, package="hexSticker", 
-        p_size=20, s_x=1, s_y=.75, s_width=1.3, s_height=1,
-        filename="images/hex_sticker.png") %>% 
-  print()
 
 fonts <- font_files()
 
 font_add("Calibri", "calibriz.ttf")
 
-sticker(
-  subplot = plot,
+sticker(subplot = plot,
   package = "Data Visualization",
-  s_width = 1.5,
-  s_height = 0.75,
-  s_x = 1,
-  s_y = 0.95,
+  s_width = 1.6,
+  s_height = 1,
+  s_x = 0.93,
+  s_y = 0.85,
   h_fill = "#8399b0",
   h_color = "black",
-  h_size = 1.75,
+  h_size = 0.85,
   spotlight = F,
   l_y = 1,
   l_x = 1,
-  l_width = 3,
-  l_height = 3,
+  l_width = 1,
+  l_height = 1,
   l_alpha = 0.3,
-  p_size = 8.5,
+  p_size = 10,
   p_color = "black",
   p_family = "Calibri",
   filename="images/hex_sticker.png") %>% 
   print()
-
-#?sticker
